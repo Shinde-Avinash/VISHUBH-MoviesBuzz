ee3a7118dc47d58e564d7a993991a57750e74ada
Avinash Shinde <85427333+Shinde-Avinash@users.noreply.github.com>
2025-12-15 19:14:10 +0530

    Update proxy targets to production server

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://vishubh-moviesbuzz-production.up.railway.app',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://vishubh-moviesbuzz-production.up.railway.app',
         changeOrigin: true,
       },
     },
